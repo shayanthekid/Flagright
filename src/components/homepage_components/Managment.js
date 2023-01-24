84a1af48dff4c369d268632f31fabed708f1e5c0
shayanthekid <sajidfayazhaniff@gmail.com>
2023-01-24 23:10:10 +0530

    started responsiveness, hero and management components done

--- src/components/homepage_components/Managment.js
+++ src/components/homepage_components/Managment.js
@@ -5,19 +5,19 @@ export default class Managment extends Component {
   render() {
     return (
         <div style={{ backgroundColor: "#f8fafe" }} className="grid grid-rows-2 justify-center items-center h-screen p-7 mt-5 -mb-20">
-            <div className="row-span-1">
-                <h1 className="font-sans font-bold text-left text-3xl p-3">
+            <div className="row-span-1 lg:pr-96 lg:mt-44 lg:mr-44">
+                <h1 className="font-sans font-bold text-left text-3xl p-3 lg:w-3/5">
 Streamline screening & case management                </h1>
                 <div className="row-span-2 text-left p-2">
-                    <div className="my-2 flex items-start p-1">
+                    <div className="my-2 flex items-start p-1 lg:py-4">
                         <div className="bg-blue-500 rounded-full w-5 h-5 flex items-center justify-center mr-2">
                             <svg className="w-5 h-5 text-white fill-current" viewBox="0 0 20 20" fill="currentColor">
                                 <path d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" />
                             </svg>
                         </div>
-                        <p >Automatically screen all new customers</p>
+                        <p className='' >Automatically screen all new customers</p>
                     </div>
-                    <div className="my-2 flex items-start p-1">
+                    <div className="my-2 flex items-start p-1 lg:py-4">
                         <div className="bg-blue-500 rounded-full w-5 h-5 flex items-center justify-center mr-2">
                             <svg className="w-5 h-5 text-white fill-current rounded-full" viewBox="0 0 20 20" fill="currentColor">
                                 <path d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" />
@@ -25,7 +25,7 @@ Streamline screening & case management                </h1>
                         </div>
                         <p>Get a complete, unified picture of risky customers and transactions</p>
                     </div>
-                    <div className="my-2 flex items-start p-1">
+                    <div className="my-2 flex items-start p-1 lg:py-4">
                         <div className="bg-blue-500 rounded-full w-5 h-5 flex items-center justify-center mr-2">
                             <svg className="w-5 h-5 text-white fill-current" viewBox="0 0 20 20" fill="currentColor">
                                 <path d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" />
@@ -33,11 +33,20 @@ Streamline screening & case management                </h1>
                         </div>
                         <p >Flexible configuration options to minimize false positives</p>
                     </div>
+                    <div className="my-2 flex items-start p-1 lg:py-4">
+                        <div className="bg-blue-500 rounded-full w-5 h-5 flex items-center justify-center mr-2">
+                            <svg className="w-5 h-5 text-white fill-current" viewBox="0 0 20 20" fill="currentColor">
+                                <path d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" />
+                            </svg>
+                        </div>
+                        <p >Enable ongoing monitoring and let Flagright inform <br className="hidden lg:block" /> you on evolving customer risks automatically</p>
+                    </div>
+                  
                 </div>
                 
             </div>
-            <div className="row-span-1 mt-10 ">
-                <img src={picture} alt="image description" className=" w-full h-full object-cover " />
+            <div className="row-span-1 mt-10 lg:row-span-2 lg:-mt-96 relative ">
+                <img src={picture} alt="image description" className=" w-full h-full object-cover  lg:w-1/2 lg:h-auto lg:ml-auto  lg:mt-32" />
             </div>
         </div>
     )
--- src/components/homepage_components/Sources.js
+++ src/components/homepage_components/Sources.js
@@ -7,18 +7,18 @@ import logo4 from '../../assets/images/logos/FCA.png';
 export default class Sources extends Component {
   render() {
     return (
-        <div className="grid grid-rows-2 bg-neutral-50  ">
+        <div className="grid grid-rows-2 bg-neutral-50 lg:p-32">
             <div className="row-span-2">
-                <h1 className="font-sans font-bold text-left text-3xl mx-10 mb-5 ">Reliable, global data sources</h1>
+                <h1 className="font-sans font-bold text-left text-3xl mx-10 mb-5 lg:text-center ">Reliable, global data sources</h1>
             </div>
             <div className="row-span-1">
-                <p className="mx-10 my-1 mb-10 text-left">We support and frequently update our data sources from global lists including OFAC, HM Treasury, UN and EU to deliver you relevant and up-to-date information</p>
+                <p className="mx-10 my-1 mb-10 text-left lg:text-center ">We support and frequently update our  data sources from global lists including OFAC, HM <br className="hidden lg:block" /> Treasury, UN and EU to deliver you relevant and up-to-date information</p>
             </div>
-            <div className="row-span-2 grid grid-cols-2 justify-center items-center  ">
-                <img src={logo1} className=" w-20 h-20 object-fit my-2 mx-auto" alt="image1" />
-                <img src={logo4} className="object-fit  w-20 h-20 my-2 mx-auto" alt="image2" />
-                <img src={logo2} className="object-fit w-20 h-20 my-2 mx-auto" alt="image2" />
-                <img src={logo3} className="object-fit  w-20 h-20 my-2 mx-auto " alt="image2" />
+            <div className="row-span-2 grid grid-cols-2 lg:grid-cols-4 justify-center items-center lg:grid-gap-1/2 lg:px-96 lg:my-20">
+                <img src={logo1} className="object-fit w-20 h-20  my-2 mx-auto lg:my-0 lg:w-16 lg:h-16" alt="image1" />
+                <img src={logo4} className="object-fit w-20 h-20 my-2 mx-auto lg:my-0 lg:w-16 lg:h-16" alt="image2" />
+                <img src={logo2} className="object-fit w-20 h-20 my-2 mx-auto lg:my-0 lg:w-16 lg:h-16" alt="image2" />
+                <img src={logo3} className="object-fit w-20 h-20 my-2 mx-auto lg:my-0 lg:w-16 lg:h-16" alt="image2" />
             </div>
         </div>
     )
